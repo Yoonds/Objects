76dace3ce728895b7feefd4050ebd7bd1884d0fe
YoonDaesung <yoondaesung@Anipens-MacBook-Pro-2.local>
2023-01-25 23:59:54 +0900

    init push

---
+++ Objects/Objects/Etc/ObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectsApp.swift
+//  Objects
+//
+//  Created by YoonDaesung on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
